75e2e4078b140c499c36d9d7abd385f3e6d6be80
Kedar <56258764+ked1108@users.noreply.github.com>
2024-07-02 01:00:00 +0530

    Update cell.h

--- cell.h
+++ cell.h
@@ -11,7 +11,7 @@
 #include <string>
 
 class cell {
-    std::map<std::string, uint> pixel;
+    std::map<std::string, int> pixel;
 public:
     bool state;
     cell(bool state, std::string pixel);
